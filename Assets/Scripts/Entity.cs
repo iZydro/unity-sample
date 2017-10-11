295a2807e395be60cc67d36fad18208c08f59fec
Isidro Gilabert <isidro.gilabert@king.com>
2017-10-11 15:24:57 +0200

    1st Commit

---
+++ Assets/Scripts/Entity.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Entity : ScriptableObject {
+	public string Name;
+	public int Age;
+	string Faction;
+	public string Occupation;
+	public int Level = 1;
+	public int Health = 2;
+	public int Strength = 1;
+	public int Magic = 0;
+	public int Defense = 0;
+	public int Damage = 1;
+	public int Armor = 0;
+	public int NoOfAttacks = 1;
+	public string Weapon;
+	public Vector2 position;
+
+	public void TakeDamage(int Amount) {
+		Health = Health - Mathf.Clamp ((Amount - Armor), 0, int.MaxValue);
+	}
+
+	public void Attack(Entity Entity) {
+		Entity.TakeDamage (Strength);
+	}
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class Player : Entity {
+	public string[] inventory;
+	public string[] skills;
+	public int money;
+}
